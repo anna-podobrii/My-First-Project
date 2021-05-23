520addd7dbce76149e01b56e525f0f618586535f
anna-podobrii <anutik268@gmail.com>
2021-05-23 15:08:02 +0300

    Initial Commit

---
+++ Anna's project/Anna_s_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anna_s_projectApp.swift
+//  Anna's project
+//
+//  Created by Anna Podobrii on 23.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Anna_s_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
